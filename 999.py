a710a4d21920275eb395abc3eb2ead903dd350d0
111111111151 <41803343+111111111151@users.noreply.github.com>
2018-07-27 11:38:54 -0700

    Add files via upload

---
+++ 999.py
@@ -0,0 +1,131 @@
+import  gym
+answer = 2
+print("you have two chances")
+for i in range(2):
+    t =  int(input("1+1="))
+    if  t  == answer:
+        print("yes")
+        print("Ok next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 6
+print("you have two chances")
+for i in range(2):
+    t =  int(input("3+3="))
+    if  t  == answer:
+        print("OK yes")
+        print("next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+    print("next")
+answer = 9
+print("you have two chances")
+for i in range(2):
+    t =  int(input("3*3="))
+    if  t  == answer:
+        print("OK yes")
+        print("next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 20
+print("you have two chances")
+for i in range(2):
+    t =  int(input("4*5="))
+    if  t  == answer:
+        print("yes")
+        print("OK next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 27
+print("you have two chances")
+for i in range(2):
+    t =  int(input("3*9="))
+    if  t  == answer:
+        print("yes")
+        print("OK next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 81
+print("you have two chances")
+for i in range(2):
+    t =  int(input("9^="))
+    if  t  == answer:
+        print("yes")
+        print("OK next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 100
+print("you have two chances")
+for i in range(2):
+    t =  int(input("4*25="))
+    if  t  == answer:
+        print("yes")
+        print("OK next")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+answer = 1256
+print("you have two chances")
+for i in range(2):
+    t =  int(input("314*4="))
+    if  t  == answer:
+        print("yes")
+        print("thank you,good bye")
+        break
+    elif t < answer:
+        print("wrong small")
+        print("you have one chances for this problem")
+    elif t > answer:
+        print("wrong big")
+        print("you have one chances for this problem")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
